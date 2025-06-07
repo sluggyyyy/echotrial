af6e5ed051228aebce1614418a76d06515ae0011
Zachary Morgan <zacharymorgannyc@gmail.com>
2025-06-07 14:27:11 -0600

    Added money pile animation system with cleanup, functions, and event handlers. Also fixed some syntax errors somewhere in there I think

--- laundry/client/initmachines.lua
+++ laundry/client/initmachines.lua
@@ -1,13 +1,15 @@
 local washingMachines = {
-    {x = 1120.27, y = -3230.05, z = 4.7},
-    {x = 1122.27, y = -3230.05, z = 4.7}
+    {x = 1120.27, y = -3230.05, z = 4.7, heading = 0.0},
+    {x = 1122.27, y = -3230.05, z = 4.7, heading = 0.0}
 }
 
 local spawnedMachines = {}
+local spawnedMoney = {}
 local machineStates = {}
 local dirtyClothesTimerThread = nil
 local nearMachine = false
 local currentMachineIndex = nil
+local moneyAnimationThreads = {}
 
 function GetNearestWashingMachine()
     local playerCoords = GetEntityCoords(PlayerPedId())
@@ -43,25 +45,31 @@ CreateThread(function()
     local openWasher = GetHashKey('washing_open')
     RequestModel(openWasher)
 
+    local moneyObject = GetHashKey('ex_cash_pile_02')
+    RequestModel(moneyObject)
+
     local attempts = 0
-    while not HasModelLoaded(closedWasher) and attempts < 50 do
+    while (not HasModelLoaded(closedWasher) or not HasModelLoaded(openWasher) or not HasModelLoaded(moneyObject)) and attempts < 50 do
         Wait(100)
         attempts = attempts + 1
     end
     
-    if HasModelLoaded(closedWasher) then
+    if HasModelLoaded(closedWasher) and HasModelLoaded(openWasher) then
         print("Custom washing machine model loaded successfully")
         for i = 1, #washingMachines do
             local pos = washingMachines[i]
             local washingMachine = CreateObject(openWasher, pos.x, pos.y, pos.z, false, false, false)
             PlaceObjectOnGroundProperly(washingMachine)
+            local moneyPile = CreateObject(moneyObject, pos.x, pos.y - 0.005, (pos.z + 0.45))
             Wait(100)
-            local coords = GetEntityCoords(washingMachine)
-            local heading = GetEntityHeading(washingMachine)
-            washingMachines[i] = {x = coords.x, y = coords.y, z = coords.z, heading = heading}
+            local finalCoords = GetEntityCoords(washingMachine)
+            local finalHeading = GetEntityHeading(washingMachine)
+            washingMachines[i] = {x = finalCoords.x, y = finalCoords.y, z = finalCoords.z, heading = finalHeading}
             FreezeEntityPosition(washingMachine, true)
             SetEntityAsMissionEntity(washingMachine, true, true)
+            SetEntityVisible(moneyPile, false, false)
             table.insert(spawnedMachines, washingMachine)
+            table.insert(spawnedMoney, moneyPile)
         end
         print("Washing machines spawned at coordinates")
     else
@@ -99,7 +107,7 @@ AddEventHandler('laundry:closeMachine', function(machineIndex)
     if DoesEntityExist(spawnedMachines[machineIndex]) then
         local pos = washingMachines[machineIndex]
         DeleteEntity(spawnedMachines[machineIndex])
-        local newMachine = CreateObject(GetHashKey('washing_closed'), 0, 0, 0, false, false, false)
+        local newMachine = CreateObject(GetHashKey('washing_closed'), pos.x, pos.y, pos.z, false, false, false)
         SetEntityCoordsNoOffset(newMachine, pos.x, pos.y, pos.z, false, false, false)
         SetEntityHeading(newMachine, pos.heading)
         FreezeEntityPosition(newMachine, true)
@@ -113,7 +121,7 @@ AddEventHandler('laundry:openMachine', function(machineIndex)
     if DoesEntityExist(spawnedMachines[machineIndex]) then
         local pos = washingMachines[machineIndex]
         DeleteEntity(spawnedMachines[machineIndex])
-        local newMachine = CreateObject(GetHashKey('washing_open'), 0, 0, 0, false, false, false)
+        local newMachine = CreateObject(GetHashKey('washing_open'), pos.x, pos.y, pos.z, false, false, false)
         SetEntityCoords(newMachine, pos.x, pos.y, pos.z, false, false, false, false)
         SetEntityHeading(newMachine, pos.heading)
         FreezeEntityPosition(newMachine, true)
@@ -146,18 +154,59 @@ AddEventHandler('laundry:startDirtyClothesPhase', function(machineIndex)
     end)
 end)
 
+RegisterNetEvent('laundry:startMoneyAnimation')
+AddEventHandler('laundry:startMoneyAnimation', function(machineIndex)
+    if moneyAnimationThreads[machineIndex] then return end
+    
+    local moneyPile = spawnedMoney[machineIndex]
+    if not DoesEntityExist(moneyPile) then return end
+    
+    SetEntityVisible(moneyPile, true, false)
+    
+    moneyAnimationThreads[machineIndex] = CreateThread(function()
+        local time = 0
+        while machineStates[machineIndex].inUse and DoesEntityExist(moneyPile) do
+            local pitchAngle = math.cos(time * 0.015) * 10
+            SetEntityRotation(moneyPile, 0, pitchAngle, 0, 2, true)
+            time = time + 1
+            Wait(5)
+        end
+        machineStates[machineIndex].inUse = false
+        SetEntityVisible(moneyPile, false, false)
+        SetEntityRotation(moneyPile, 0, 0, 0, 2, true)
+        moneyAnimationThreads[machineIndex] = nil
+    end)
+end)
+
+
+RegisterNetEvent('laundry:stopMoneyAnimation')
+AddEventHandler('laundry:stopMoneyAnimation', function(machineIndex)
+    if machineStates[machineIndex] then
+        machineStates[machineIndex].inUse = false
+        print("Stopping money animation for machine", machineIndex)
+    end
+end)
+
 RegisterNetEvent('laundry:showMessage')
 AddEventHandler('laundry:showMessage', function(message, machineIndex)
     TriggerEvent('chat:addMessage', {
         args = {message}
     })
     
-    if machineIndex and machineStates[machineIndex] then
+    print("Message received:", message, "Machine Index:", machineIndex)
+    
+    local targetMachine = machineIndex or currentMachineIndex
+    
+    if targetMachine and machineStates[targetMachine] then
         if string.find(message, "Washing starting") then
-            machineStates[machineIndex].inUse = true
+            print("Starting washing animation for machine", targetMachine)
+            machineStates[targetMachine].inUse = true
+            StartMoneyAnimation(targetMachine)
         elseif string.find(message, "Washing finished") then
-            machineStates[machineIndex].inUse = false
+            machineStates[targetMachine].inUse = false
         end
+    else
+        print("No machine index or invalid machine state")
     end
 end)
 
@@ -168,6 +217,12 @@ AddEventHandler('onResourceStop', function(resourceName)
                 DeleteEntity(spawnedMachines[i])
             end
         end
-        print("Cleaned up washing machines")
+
+        for i = 1, #spawnedMoney do
+            if DoesEntityExist(spawnedMoney[i]) then
+                DeleteEntity(spawnedMoney[i])
+            end
+        end
+        print("Cleaned up washing machines and money")
     end
 end)
