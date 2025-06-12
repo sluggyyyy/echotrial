29ffc4a8d54353b2cf81448b9323b41099b24080
Zachary Morgan <zacharymorgannyc@gmail.com>
2025-06-11 22:08:54 -0600

    Didn't really update much here except TS compatibility

--- moneycounter/client/initcounter.lua
+++ moneycounter/client/initcounter.lua
@@ -1,21 +1,31 @@
+
 local moneyCounters = {
     {x = 1110.0, y = -3230.0, z = 4.7, heading = 0.0}
 }
 
 local spawnedCounters = {}
 local counterStates = {}
+local proximityUpdateThread = nil
+local countingActive = false
+local activeCounterIndex = nil
 
 function GetNearestMoneyCounter()
     local playerCoords = GetEntityCoords(PlayerPedId())
     
     for i = 1, #moneyCounters do
         local counter = moneyCounters[i]
-        local distance = GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, counter.x, counter.y, counter.z, true)
+        
+        local distance = GetDistanceBetweenCoords(
+            playerCoords.x, playerCoords.y, playerCoords.z,
+            counter.x, counter.y, counter.z,
+            true
+        )
         
         if distance < 2.0 then
             return i
         end
     end
+    
     return nil
 end
 
@@ -44,53 +54,138 @@ CreateThread(function()
     if HasModelLoaded(counterModel) then
         for i = 1, #moneyCounters do
             local pos = moneyCounters[i]
+            
             local counter = CreateObject(counterModel, pos.x, pos.y, pos.z, false, false, false)
+            
             PlaceObjectOnGroundProperly(counter)
+            
             Wait(100)
             local finalCoords = GetEntityCoords(counter)
-            SetEntityHeading(counter, pos.heading)
+            local finalHeading = GetEntityHeading(counter)
+            
+            moneyCounters[i] = {
+                x = finalCoords.x, 
+                y = finalCoords.y, 
+                z = finalCoords.z, 
+                heading = finalHeading
+            }
+            
             FreezeEntityPosition(counter, true)
             SetEntityAsMissionEntity(counter, true, true)
+            
             table.insert(spawnedCounters, counter)
         end
-        print("Money counters spawned")
-    else
-        print("Failed to load money counter model")
     end
 end)
 
 CreateThread(function()
     while true do
         Wait(0)
-        local currentCounterIndex = GetNearestMoneyCounter()
-
-        if currentCounterIndex then
-            local counter = counterStates[currentCounterIndex]
+        
+        local nearestCounter = GetNearestMoneyCounter()
+        
+        if nearestCounter then
+            local counter = counterStates[nearestCounter]
+            
             if not counter.inUse then
+                
                 SetTextComponentFormat("STRING")
-                AddTextComponentString("Press E to bundle your cash.")
+                AddTextComponentString("Press E to count money.")
                 DisplayHelpTextFromStringLabel(0, 0, 1, -1)
 
                 if IsControlJustReleased(0, 38) then
-                    TriggerServerEvent('moneycounter:processCash', currentCounterIndex)
+                    
+                    TriggerServerEvent('moneycounter:startCounting', nearestCounter)
                 end
             else
                 SetTextComponentFormat("STRING")
-                AddTextComponentString("Cannot stack cash right now...")
+                AddTextComponentString("Money counter is busy...")
                 DisplayHelpTextFromStringLabel(0, 0, 1, -1)
             end
+        end
+    end
+end)
+
+RegisterNetEvent('moneycounter:startCountingAnimation')
+AddEventHandler('moneycounter:startCountingAnimation', function(counterIndex)
+    countingActive = true
+    activeCounterIndex = counterIndex
+    
+    SendNUIMessage({
+        type = 'playSound',
+        sound = 'counting_loop'
+    })
+    
+    if proximityUpdateThread then
+        proximityUpdateThread = nil
+    end
+    
+    proximityUpdateThread = CreateThread(function()
+        while countingActive and activeCounterIndex == counterIndex do
+            local playerCoords = GetEntityCoords(PlayerPedId())
+            local counter = moneyCounters[counterIndex]
+            
+            local distance = GetDistanceBetweenCoords(
+                playerCoords.x, playerCoords.y, playerCoords.z,
+                counter.x, counter.y, counter.z, true
+            )
+            
+            if distance > 10.0 then
+                TriggerServerEvent('moneycounter:cancelCounting')
+                break
+            end
+            
+            Wait(500)
+        end
+        proximityUpdateThread = nil
+    end)
+end)
+
+RegisterNetEvent('moneycounter:stopCountingAnimation')
+AddEventHandler('moneycounter:stopCountingAnimation', function(counterIndex)
+    countingActive = false
+    activeCounterIndex = nil
+    
+    SendNUIMessage({
+        type = 'stopSound',
+        sound = 'counting_loop'
+    })
+    
+    SendNUIMessage({
+        type = 'playSound',
+        sound = 'beep'
+    })
+    
+    if counterStates[counterIndex] then
+        counterStates[counterIndex].inUse = false
+    end
+end)
+
+RegisterNetEvent('moneycounter:showMessage')
+AddEventHandler('moneycounter:showMessage', function(message, counterIndex)
+    TriggerEvent('chat:addMessage', {
+        args = {message}
+    })
+    
+    local targetCounter = counterIndex or GetNearestMoneyCounter()
+    
+    if targetCounter and counterStates[targetCounter] then
+        if string.find(message, "Counting money") then
+            counterStates[targetCounter].inUse = true
+        elseif string.find(message, "Money counting complete") then
+            counterStates[targetCounter].inUse = false
         end
     end
 end)
 
 AddEventHandler('onResourceStop', function(resourceName)
     if GetCurrentResourceName() == resourceName then
+        
         for i = 1, #spawnedCounters do
             if DoesEntityExist(spawnedCounters[i]) then
                 DeleteEntity(spawnedCounters[i])
             end
         end
-        print("Cleaned up money counters")
+        
     end
 end)
-
--- moneycounter/server/processcash.lua
+++ moneycounter/server/processcash.lua
@@ -1,23 +1,104 @@
-RegisterNetEvent('moneycounter:processCash')
-AddEventHandler('moneycounter:processCash', function(counterIndex)
+
+
+local playerInventories = {}
+
+local activeCounting = {}
+
+local function getPlayerIdentifier(source)
+    local identifier = GetPlayerIdentifier(source, 0)
+    
+    return identifier or tostring(source)
+end
+
+local function getPlayerInventory(source)
+    local playerId = getPlayerIdentifier(source)
+    
+    if not playerInventories[playerId] then
+        playerInventories[playerId] = {
+            cashStack = 5,
+            cashRoll = 1,
+            looseNotes = 10
+        }
+    end
+    
+    return playerInventories[playerId]
+end
+
+local function getAmount(source, item)
+    local inventory = getPlayerInventory(source)
+    
+    if inventory[item] ~= nil then
+        return inventory[item]
+    end
+    
+    return 0
+end
+
+local function addItem(source, item, amount)
+    local inventory = getPlayerInventory(source)
+    
+    if inventory[item] ~= nil then
+        inventory[item] = inventory[item] + amount
+        return true
+    end
+    
+    return false
+end
+
+local function removeItem(source, item, amount)
+    local inventory = getPlayerInventory(source)
+    
+    if inventory[item] ~= nil and inventory[item] >= amount then
+        inventory[item] = inventory[item] - amount
+        return true
+    end
+    
+    return false
+end
+
+local function hasItem(source, item, amount)
+    local inventory = getPlayerInventory(source)
+    
+    return inventory[item] ~= nil and inventory[item] >= amount
+end
+
+local function TriggerEventNearby(eventName, playerId, radius, ...)
+    local playerCoords = GetEntityCoords(GetPlayerPed(playerId))
+    
+    local players = GetPlayers()
+    
+    for _, nearbyPlayer in ipairs(players) do
+        local nearbyCoords = GetEntityCoords(GetPlayerPed(nearbyPlayer))
+        
+        local distance = #(playerCoords - nearbyCoords)
+        
+        if distance <= radius then
+            TriggerClientEvent(eventName, nearbyPlayer, ...)
+        end
+    end
+end
+
+RegisterNetEvent('moneycounter:startCounting')
+AddEventHandler('moneycounter:startCounting', function(counterIndex)
     local playerId = source
     
-    local hasCashStack = exports['laundry']:hasItem(playerId, 'cashStack', 1)
-    local hasCashRoll = exports['laundry']:hasItem(playerId, 'cashRoll', 1)
-    local hasLooseNotes = exports['laundry']:hasItem(playerId, 'looseNotes', 1)
+    local hasCashStack = hasItem(playerId, 'cashStack', 1)
+    local hasCashRoll = hasItem(playerId, 'cashRoll', 1)
+    local hasLooseNotes = hasItem(playerId, 'looseNotes', 1)
     
-    local cashStackCount = exports['laundry']:getAmount(playerId, 'cashStack') or 0
-    local cashRollCount = exports['laundry']:getAmount(playerId, 'cashRoll') or 0
-    local looseNotesCount = exports['laundry']:getAmount(playerId, 'looseNotes') or 0
+    local cashStackCount = getAmount(playerId, 'cashStack') or 0
+    local cashRollCount = getAmount(playerId, 'cashRoll') or 0
+    local looseNotesCount = getAmount(playerId, 'looseNotes') or 0
     
     local notesPerRoll = 7
     local rollsPerStack = 2
-
+    
     if cashRollCount < rollsPerStack and looseNotesCount < notesPerRoll then
         TriggerClientEvent('chat:addMessage', playerId, {args = {"You don't have enough dirty money to process."}})
         return
     end
 
+    
     local newRollsFromNotes = math.floor(looseNotesCount / notesPerRoll)
     local remainingNotes = looseNotesCount % notesPerRoll
     
@@ -31,24 +112,103 @@ AddEventHandler('moneycounter:processCash', function(counterIndex)
     local totalItems = cashStackCount + cashRollCount + looseNotesCount
     local processingTime = totalItems * 1000
     
-    exports['laundry']:removeItem(playerId, 'cashStack', cashStackCount)
-    exports['laundry']:removeItem(playerId, 'cashRoll', cashRollCount)
-    exports['laundry']:removeItem(playerId, 'looseNotes', looseNotesCount)
+    activeCounting[playerId] = {
+        cashStack = cashStackCount,
+        cashRoll = cashRollCount,
+        looseNotes = looseNotesCount,
+        counterIndex = counterIndex
+    }
+    
+    removeItem(playerId, 'cashStack', cashStackCount)
+    removeItem(playerId, 'cashRoll', cashRollCount)
+    removeItem(playerId, 'looseNotes', looseNotesCount)
     
     TriggerClientEvent('chat:addMessage', playerId, {args = {"Processing " .. totalItems .. " items..."}})
     
-    Wait(processingTime)
+    TriggerEventNearby('moneycounter:startCountingAnimation', playerId, 50.0, counterIndex, processingTime)
     
-    if totalStacks > 0 then
-        exports['laundry']:addItem(playerId, 'cashStack', totalStacks)
+    Wait(processingTime - 1000)
+    
+    if activeCounting[playerId] then
+        TriggerEventNearby('moneycounter:stopCountingAnimation', playerId, 50.0, counterIndex)
+        Wait(1000)
+        
+        if activeCounting[playerId] then
+            if totalStacks > 0 then
+                addItem(playerId, 'cashStack', totalStacks)
+            end
+            if remainingRolls > 0 then
+                addItem(playerId, 'cashRoll', remainingRolls)
+            end
+            if remainingNotes > 0 then
+                addItem(playerId, 'looseNotes', remainingNotes)
+            end
+            
+            local message = string.format("Money counting finished! Result: %d stacks, %d rolls, %d notes", totalStacks, remainingRolls, remainingNotes)
+            TriggerClientEvent('chat:addMessage', playerId, {args = {message}})
+            
+            TriggerEventNearby('moneycounter:finishCounting', playerId, 50.0, counterIndex)
+            
+            activeCounting[playerId] = nil
+        end
     end
-    if remainingRolls > 0 then
-        exports['laundry']:addItem(playerId, 'cashRoll', remainingRolls)
+end)
+
+RegisterCommand('checkcash', function(source, args)
+    if source == 0 then return end
+
+    local inventory = getPlayerInventory(source)
+
+    TriggerClientEvent('chat:addMessage', source, {
+        args = {'=== CASH INVENTORY ==='}
+    })
+    
+    for item, amount in pairs(inventory) do
+        TriggerClientEvent('chat:addMessage', source, {
+            args = {string.format('%s: %d', item, amount)}
+        })
     end
-    if remainingNotes > 0 then
-        exports['laundry']:addItem(playerId, 'looseNotes', remainingNotes)
+end, false)
+
+RegisterCommand('givecash', function(source, args)
+    if source == 0 then return end
+
+    local item = args[1]
+    local amount = tonumber(args[2]) or 1
+    
+    if not item then
+        TriggerClientEvent('chat:addMessage', source, {
+            args = {'^1[Error] Usage: /givecash itemname amount'}
+        })
+        return
     end
     
-    local message = string.format("Money counting finished! Result: %d stacks, %d rolls, %d notes", totalStacks, remainingRolls, remainingNotes)
-    TriggerClientEvent('chat:addMessage', playerId, {args = {message}})
+    if addItem(source, item, amount) then
+        TriggerClientEvent('chat:addMessage', source, {
+            args = {string.format('^2[Success] Added %d %s to inventory', amount, item)}
+        })
+    else
+        TriggerClientEvent('chat:addMessage', source, {
+            args = {string.format('^1[Error] Invalid item: %s', item)}
+        })
+    end
+end, false)
+
+RegisterNetEvent('moneycounter:cancelCounting')
+AddEventHandler('moneycounter:cancelCounting', function()
+    local playerId = source
+    
+    if activeCounting[playerId] then
+        local data = activeCounting[playerId]
+        
+        addItem(playerId, 'cashStack', data.cashStack)
+        addItem(playerId, 'cashRoll', data.cashRoll)
+        addItem(playerId, 'looseNotes', data.looseNotes)
+        
+        TriggerEventNearby('moneycounter:stopCountingAnimation', playerId, 50.0, data.counterIndex)
+        
+        TriggerClientEvent('chat:addMessage', playerId, {args = {'Money counting cancelled - you moved too far away!'}})
+        
+        activeCounting[playerId] = nil
+    end
 end)
